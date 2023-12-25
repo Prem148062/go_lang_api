add48df0fedb3c168b47b4fba667e3f1d1c52d42
lolipopwitthawasDEVPz <qwertyuiopasdfgh112233@gmail.com>
2023-12-25 16:54:08 +0700

    postgres 101

--- pkg/database/migrations/000001_lsa_sql_migrations.down.sql
+++ pkg/database/migrations/000001_lsa_sql_migrations.down.sql
@@ -1,15 +1,27 @@
 BEGIN;
 
-DROP TRIGGER IF EXISTS set_update_at_timestamp_users_table ON "users";
-DROP TRIGGER IF EXISTS set_update_at_timestamp_oauth_table ON "oauth";
-DROP TRIGGER IF EXISTS set_update_at_timestamp_products_table ON "products";
-DROP TRIGGER IF EXISTS set_update_at_timestamp_images_table ON "images";
-DROP TRIGGER IF EXISTS set_update_at_timestamp_orders_table ON "orders";
+DROP TRIGGER IF EXISTS set_updated_at_timestamp_users_table ON "users";
+DROP TRIGGER IF EXISTS set_updated_at_timestamp_oauth_table ON "oauth";
+DROP TRIGGER IF EXISTS set_updated_at_timestamp_products_table ON "products";
+DROP TRIGGER IF EXISTS set_updated_at_timestamp_images_table ON "images";
+DROP TRIGGER IF EXISTS set_updated_at_timestamp_orders_table ON "orders";
 
-CREATE TRIGGER set_update_at_timestamp_users_table BEFORE UPDATE ON "users" FOR EACH ROW EXCUTE PROCEDURE set_update_at_column();
-CREATE TRIGGER set_update_at_timestamp_oauth_table BEFORE UPDATE ON "oauth" FOR EACH ROW EXCUTE PROCEDURE set_update_at_column();
-CREATE TRIGGER set_update_at_timestamp_products_table BEFORE UPDATE ON "products" FOR EACH ROW EXCUTE PROCEDURE set_update_at_column();
-CREATE TRIGGER set_update_at_timestamp_images_table BEFORE UPDATE ON "images" FOR EACH ROW EXCUTE PROCEDURE set_update_at_column();
-CREATE TRIGGER set_update_at_timestamp_orders_table BEFORE UPDATE ON "orders" FOR EACH ROW EXCUTE PROCEDURE set_update_at_column();
+DROP FUNCTION IF EXISTS set_updated_at_column();
+
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "oauth" CASCADE;
+DROP TABLE IF EXISTS "roles" CASCADE;
+DROP TABLE IF EXISTS "products" CASCADE;
+DROP TABLE IF EXISTS "categories" CASCADE;
+DROP TABLE IF EXISTS "products_categories" CASCADE;
+DROP TABLE IF EXISTS "images" CASCADE;
+DROP TABLE IF EXISTS "orders" CASCADE;
+DROP TABLE IF EXISTS "products_orders" CASCADE;
+
+DROP SEQUENCE IF EXISTS users_id_seq;
+DROP SEQUENCE IF EXISTS products_id_seq;
+DROP SEQUENCE IF EXISTS orders_id_seq;
+
+DROP TYPE IF EXISTS "order_status";
 
 COMMIT;
